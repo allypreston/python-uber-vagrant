1886349aa3a2b02baf41bf01ecfde59090938e0d
Ally Preston <APreston@spartaglobal.com>
2019-08-18 16:10:50 +0100

    successfully pulled the cookbooks and ran the app with no errors

---
+++ berks-cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook:: nginx
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [:enable, :start]
+end
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  variables proxy_port: node['nginx']['proxy_port']
+  notifies :restart, 'service[nginx]'
+end
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies :restart, 'service[nginx]'
+end
